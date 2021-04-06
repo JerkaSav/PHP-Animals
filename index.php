fe5537e452e332953ca0bb67ee7b733e948f43e7
Fanny Mårtensson <fanny.martensson@gmail.com>
2021-04-06 10:30:23 +0200

    directory to database animals

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php echo 'Hello you!';
- 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+$dbh = new PDO('mysql:host=localhost;dbname=zoo;port=3306;charset=utf8;', "animals", "animals");
+?>
