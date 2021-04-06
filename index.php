2e908efe716dea01175f06d6ea8b9050719f31a8
Erik Sävström <savstromerik@gmail.com>
2021-04-06 15:48:46 +0200

    yess

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 
 
-$dbh = new PDO('mysql:host=localhost;dbname=zoo;port=3306;charset=utf8;', "animals", "animals",) or die ($mysqli->connection_error);
+$dbh = new PDO('mysql:host=localhost;dbname=zoo;port=3306;charset=utf8;', "animals", "animals", ) or die($mysqli->connection_error);
 
 $query = "SELECT * FROM animals WHERE ':id' < 100";
 $statement = $dbh->prepare($query, array(PDO::FETCH_ASSOC));
@@ -14,52 +14,54 @@ $statement->execute(array(':id' => 10));
 $result = $statement->fetchAll();
 
 $selectedName = str_replace('-', ' ', $_POST['animals']);
+
 $querySelectByName = 'SELECT * FROM animals WHERE name = ?';
 $statementByName = $dbh->prepare($querySelectByName, array(PDO::FETCH_ASSOC));
 $statementByName->execute(array($selectedName));
 $resultByName = $statementByName->fetchAll();
 
 
-$path_dir = "uploads/";
-$chosen_image = $path_dir . basename($_FILES["uploadedFile"]["name"]);
-$approvedUpload = 1;
-$typeOfImage = strtolower(pathinfo($chosen_image,PATHINFO_EXTENSION));
 
 // check to see that the file is an image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
-  if($check !== false) {
-    echo "Filen är en bild- " . $check["mime"] . ".";
+if (isset($_POST["submit"])) {
+    $path_dir = "uploads/";
+    $chosen_image = $path_dir . basename($_FILES["uploadedFile"]["name"]);
     $approvedUpload = 1;
-  } else {
-    echo "Filen är inte en bild.";
-    $approvedUpload = 0;
-  }
-  // File size 
-if ($_FILES["uploadedFile"]["size"] > 500000) {
-  echo "Nu har du lagt upp en för stor file för våra djur att hantera!";
-  $approvedUpload = 0;
-}
+    $typeOfImage = strtolower(pathinfo($chosen_image, PATHINFO_EXTENSION));
+    $check = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
+    if ($check !== false) {
+        echo "Filen är en bild- " . $check["mime"] . ".";
+        $approvedUpload = 1;
+    } else {
+        echo "Filen är inte en bild.";
+        $approvedUpload = 0;
+    }
+    // File size
+    if ($_FILES["uploadedFile"]["size"] > 500000) {
+        echo "Nu har du lagt upp en för stor file för våra djur att hantera!";
+        $approvedUpload = 0;
+    }
 
-// Limit file type
-if($typeOfImage != "jpg" && $typeOfImage != "png" && $typeOfImage != "jpeg"
+    // Limit file type
+    if ($typeOfImage != "jpg" && $typeOfImage != "png" && $typeOfImage != "jpeg"
  ) {
-  echo "Ledsen, bara JPG-, JPEG-, PNG- & GIF-filer är tillåtna.";
-  $approvedUpload = 0;
-}
+        echo "Ledsen, bara JPG-, JPEG-, PNG- & GIF-filer är tillåtna.";
+        $approvedUpload = 0;
+    }
+    // Check if $approvedUpload is set to 0 by an error
+    if ($approvedUpload == 0) {
+        echo "Ledsen, filen blev inte uppladdad.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $chosen_image)) {
+            echo "Filen ". htmlspecialchars(basename($_FILES["uploadedFile"]["name"])). " har blivit uppladdad.";
+        } else {
+            echo "Något blev fel med uppladdningen.";
+        }
+    }
 }
 
-// Check if $approvedUpload is set to 0 by an error
-if ($approvedUpload == 0) {
-  echo "Ledsen, filen blev inte uppladdad.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $chosen_image)) {
-    echo "Filen ". htmlspecialchars( basename( $_FILES["uploadedFile"]["name"])). " har blivit uppladdad.";
-  } else {
-    echo "Något blev fel med uppladdningen.";
-  }
-}
+
 
 ?>
 
@@ -136,13 +138,13 @@ if ($approvedUpload == 0) {
   </table>
 
 
-<form action="" method="post" enctype="multipart/form-data">
-  Välj en bild att ladda upp:
-  <input type="file" name="uploadedFile" id="uploadedFile">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+  <form action="" method="post" enctype="multipart/form-data">
+    Välj en bild att ladda upp:
+    <input type="file" name="uploadedFile" id="uploadedFile">
+    <input type="submit" value="Upload Image" name="submit">
+  </form>
 
-<img src="<?php 
+  <img src="<?php
 echo $chosen_image
 ?>">
 
