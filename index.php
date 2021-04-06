22bd079e41c1c303cda6e35cb077ed8d02500563
Erik Sävström <savstromerik@gmail.com>
2021-04-06 10:36:13 +0200

    display names of animals

--- index.php
+++ index.php
@@ -7,3 +7,12 @@ error_reporting(E_ALL);
 
 
 $dbh = new PDO('mysql:host=localhost;dbname=zoo;port=3306;charset=utf8;', "animals", "animals");
+
+$query = "SELECT name FROM animals WHERE ':id' < 10";
+$statement = $dbh->prepare($query, array(PDO::FETCH_ASSOC));
+$statement->execute(array(':id' => 10));
+$result = $statement->fetchAll();
+
+foreach ($result as $animal) {
+    echo $animal['name'] . "<br />";
+}
