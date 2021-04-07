9e95a422c26dbfd119f1260c908079cf309f7690
Fanny Mårtensson <fanny.martensson@gmail.com>
2021-04-07 13:43:36 +0200

    update form

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $result = $statement->fetchAll();
 
 $selectedName = '';
 $column = '';
-if (isset($_POST['sortByName']) || isset($_POST['fromName'])) {
+if (isset($_POST['sortByName'])) {
     if (!empty($_POST['userInput']) && !empty($_POST['column'])) {
         $selectedName = $_POST['userInput'];
    
@@ -28,12 +28,6 @@ if (isset($_POST['sortByName']) || isset($_POST['fromName'])) {
         }
         $statementByName = $dbh->prepare($querySelectByName, array(PDO::FETCH_ASSOC));
         $statementByName->execute(array("$selectedName%"));
-        $resultByName = $statementByName->fetchAll();
-    } else {
-        $selectedName = $_POST['animals'];
-        $querySelectByName = 'SELECT * FROM animals WHERE name = ?';
-        $statementByName = $dbh->prepare($querySelectByName, array(PDO::FETCH_ASSOC));
-        $statementByName->execute(array($selectedName));
         $resultByName = $statementByName->fetchAll();
     }
 }
@@ -98,43 +92,36 @@ if (isset($_POST["submit"])) {
 </head>
 
 <body>
-  <label for="search">Välj ett alternativ och skriv ett sökord</label>
+
+  <label for="names-animals">Välj ett djur</label>
   <form action="" method="post">
-    <select id="search" name='column'>
+    <select id="names-animals" name='column'>
       <option value='name'>Namn</option>
       <option value='category'>Kategori</option>
       <input type="text" name='userInput'>
+      <?php
+      
+          // foreach ($result as $animal) {
+          //     if ($animal['name'] == $selectedName) {
+          //         echo
+          //     '<option Selected value='
+          //     .str_replace(' ', '-', $animal['name'])
+          //     .'>'
+          //     .$animal['name']
+          //     .'</option>';
+          //     } else {
+          //         echo
+          //     '<option value='
+          //     .str_replace(' ', '-', $animal['name'])
+          //     .'>'
+          //     .$animal['name']
+          //     .'</option>';
+          //     }
+          // }
+          ?>
       <input type="submit" value="Skicka in" name="sortByName">
     </select>
   </form>
-  <br>
-  <form action="" method="post">
-    <label for="names-animals">Eller: välj ett djur</label> <br>
-    <select id="names-animals" name='animals'>
-      <?php
-      
-           foreach ($result as $animal) {
-               if ($animal['name'] == $selectedName) {
-                   echo
-               '<option Selected value='
-               .str_replace(' ', '-', $animal['name'])
-               .'>'
-               .$animal['name']
-               .'</option>';
-               } else {
-                   echo
-               '<option value='
-               .str_replace(' ', '-', $animal['name'])
-               .'>'
-               .$animal['name']
-               .'</option>';
-               }
-           }
-          ?>
-    </select>
-    <input type="submit" name="fromName" value='Skicka in'>
-  </form>
-
   <table>
     <tr>
       <th>
@@ -151,19 +138,17 @@ if (isset($_POST["submit"])) {
       </th>
     </tr>
     <?php
-    if (isset($_POST['sortByName']) || isset($_POST['fromName'])) {
-        if (!empty($_POST['userInput']) && !empty($_POST['column']) || !empty($_POST['animals'])) {
-            foreach ($resultByName as $animal) {
-                echo '<tr>'
+    if (isset($_POST['sortByName'])) {
+      if (!empty($_POST['userInput']) && !empty($_POST['column'])) {
+          foreach ($resultByName as $animal) {
+              echo '<tr>'
               .'<td>'.$animal['id'] .'</td>'
               .'<td>'.$animal['name'] .'</td>'
               .'<td>'.$animal['category'] .'</td>'
               .'<td>'.$animal['birthday'] .'</td>'
               .'</tr>';
-            }
-        } else {
-            echo "Du måste skriva något!";
-        }
+          }
+      }
     }
           ?>
 
@@ -215,6 +200,17 @@ if (isset($_POST['save'])) {
 
         echo "<meta http-equiv='refresh' content='0'>";
     }
+   
+      if (empty($_POST['name'])) {
+          echo '<br>'.'<span>'.'Du måste fylla i namnet!'.'</span>';
+      }
+      if (empty($_POST['category'])) {
+          echo '<br>'.'<span>'.'Du måste fylla i kategori!'.'</span>';
+      }
+      if (empty($_POST['birthday'])) {
+          echo '<br>'.'<span>'.'Du måste fylla i födelsedatum!'.'</span>';
+      }
+      
 }
 
 ?>
